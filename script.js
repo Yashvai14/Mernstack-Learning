a720ab7590a7d54db4c73a28abce936c75e44b19
Yashvai14 <vaidyayash36@gmail.com>
2025-10-23 21:53:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "harry";
+// console.log(name.length);
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.slice(2));
+// console.log(name.replace("har", "per"));
+
+let friend = "rohan";
+console.log(name.concat("is a friend of ", friend, " ok"));
